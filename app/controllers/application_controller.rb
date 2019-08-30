9093339f8e00e8f3ccc82bf77112c97f7cdea195
Sara Harvey <saraharveynyc@gmail.com>
2019-08-30 16:41:59 -0400

    display results via string interpolation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,30 +7,11 @@ require 'yaml/store'
   end
 
   get "/" do
+    @title = 'Calendar List'
     erb :index
   end
 
-  post '/add' do
-    @title = 'Got it!'
-    @event = params['event'] 
-    @date = params['event_date']
-    @submitted_by = params['submitted_by']
-
-    @store = YAML::Store.new 'events.yml'
-    @store.transaction do 
-      @store['events'] = {}
-      @store['events'][@event]
-      @store['events'][@date]
-      @store['events'][@submitted_by]
-    end
-    erb :add
-  end
-
-  get '/results' do
-    @title = 'Results so far:'
-    @store = YAML::Store.new 'events.yml'
-    @events = @store.transaction { @store['events'] }
-    
-    erb :results
+  post '/add' do 
+    "#{params[:event_date]} — #{params[:name]}, submitted by: #{params[:submitted_by]}" 
   end
 end
